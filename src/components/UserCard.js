02c5eb2fc9a4378bd6cf00984ced268db88a0a14
mayurlalwani <lalwanimayur06@gmail.com>
2022-04-03 11:28:15 +0530

    add space between name and image

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -14,7 +14,7 @@ const UserCard = ({ userData }) => {
         />
       </div>
       <div className="bottom-section">
-        <h3>{userData.name}</h3>
+        <h3 style={{ marginTop: 25 }}>{userData.name}</h3>
         <span className="user-info">{userData.email}</span>
         <span className="user-info">{userData.phone}</span>
         <span className="user-info">{userData.country}</span>
